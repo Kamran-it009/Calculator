564298e16822f97d6288c17fe82665d773f96d62
Kamran-it009 <kamran.it009@gmail.com>
2024-09-06 18:23:26 +0500

    Initial

---
+++ hell2.py
@@ -0,0 +1,5 @@
+n =  20
+print('The type of n:', type(n))
+f = float(n)
+print('The type of f:', type(f))
+print('The type of n:', type(str(n)))
---
+++ hello.py
@@ -0,0 +1,5 @@
+print('This is Batch-18')
+print('This is Batch-17', end='|')
+print('This is Batch-16', end='|')
+print('This is Batch-15', end='|')
+
