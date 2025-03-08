c295b37a0387a16eb31a5bc1340bb822eadebe88
Kamran-it009 <kamran.it009@gmail.com>
2025-03-08 12:53:02 +0500

    updated

--- hell2.py
+++
@@ -1,5 +0,0 @@
-n =  20
-print('The type of n:', type(n))
-f = float(n)
-print('The type of f:', type(f))
-print('The type of n:', type(str(n)))
--- hello.py
+++
@@ -1,5 +0,0 @@
-print('This is Batch-18')
-print('This is Batch-17', end='|')
-print('This is Batch-16', end='|')
-print('This is Batch-15', end='|')
-
